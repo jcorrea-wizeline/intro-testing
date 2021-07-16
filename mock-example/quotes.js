3deb5015120eb886bc0589dc1f1297eba891c665
jcorrea <juan.correa@wizeline.com>
2021-07-16 08:46:43 -0500

    added async test examples

---
+++ mock-example/quotes.js
@@ -0,0 +1,9 @@
+module.exports.getQuote = () => {
+  return new Promise((resolve) => {
+    setTimeout(
+      () =>
+        resolve("Don’t believe everything you read on the internet, Buddha"),
+      1000
+    );
+  });
+};
---
+++ mock-example/quotes.test.js
@@ -0,0 +1,27 @@
+const { getQuote } = require("./quotes")
+
+describe("getQuote", () => {
+  test("resolves the quote version callback", (done) => {
+    getQuote().then((quote) => {
+      expect(quote).toBe(
+        "Don’t believe everything you read on the internet, Buddha"
+      )
+      done()
+    })
+  })
+
+  test("resolves the quote version returning a promise", () => {
+    return getQuote().then((quote) => {
+      expect(quote).toBe(
+        "Don’t believe everything you read on the internet, Buddha"
+      )
+    })
+  })
+
+  test("resolves the quote version async await", async () => {
+    const quote = await getQuote();
+    expect(quote).toBe(
+      "Don’t believe everything you read on the internet, Buddha"
+    )
+  })
+})
