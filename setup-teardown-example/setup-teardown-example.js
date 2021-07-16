217138e8ac88bcdd28eea19e86a2dcbea3d2ed3c
jcorrea <juan.correa@wizeline.com>
2021-07-16 08:46:52 -0500

    added setup - teardown example

---
+++ setup-teardown-example/setup-teardown-example.js
@@ -0,0 +1,23 @@
+beforeAll(() => {
+  console.log("beforeAll");
+});
+
+beforeEach(() => {
+  console.log("beforeEach");
+});
+
+afterEach(() => {
+  console.log("afterEach");
+});
+
+afterAll(() => {
+  console.log("afterAll");
+});
+
+test("first test", () => {
+  console.log("first test");
+});
+
+test("second test", () => {
+  console.log("second test");
+});
