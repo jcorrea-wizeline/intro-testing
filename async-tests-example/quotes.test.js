88ab42fba30d327d5d85a15979509e5eade76882
jcorrea <juan.correa@wizeline.com>
2021-07-16 09:28:07 -0500

    updated tests

--- async-tests-example/quotes.test.js
+++ async-tests-example/quotes.test.js
@@ -1,27 +1,9 @@
-const {getQuote} = require('./quotes')
+// const {getQuote} = require('./quotes')
 
 describe('getQuote', () => {
-  test('resolves the quote version callback', (done) => {
-    getQuote().then((quote) => {
-      expect(quote).toBe(
-        'Don’t believe everything you read on the internet, Buddha',
-      )
-      done()
-    })
-  })
+  test.todo('resolves the quote version callback')
 
-  test('resolves the quote version returning a promise', () => {
-    return getQuote().then((quote) => {
-      expect(quote).toBe(
-        'Don’t believe everything you read on the internet, Buddha',
-      )
-    })
-  })
+  test.todo('resolves the quote version returning a promise')
 
-  test('resolves the quote version async await', async () => {
-    const quote = await getQuote()
-    expect(quote).toBe(
-      'Don’t believe everything you read on the internet, Buddha',
-    )
-  })
+  test.todo('resolves the quote version async await')
 })
--- mock-example/number.facts.test.js
+++ mock-example/number.facts.test.js
@@ -1,28 +1,6 @@
-const {getRandomNumberFact} = require('./numbers-facts')
-const {getRandomNumberFactService} = require('./number-facts-service')
+// const {getRandomNumberFact} = require('./numbers-facts')
+// const {getRandomNumberFactService} = require('./number-facts-service')
 
-jest.mock('./number-facts-service')
+test.todo('returns a random number fact')
 
-const fakeResponse = {
-  text: '652 is the year that Khazaria becomes an independent state (approximate date).',
-}
-
-beforeEach(() => {
-  getRandomNumberFactService.mockClear()
-})
-
-test('getRandomNumberFact', async () => {
-  getRandomNumberFactService.mockReturnValueOnce(fakeResponse)
-
-  const result = await getRandomNumberFact()
-  expect(getRandomNumberFactService).toHaveBeenCalledTimes(1)
-  expect(result.text).toBe(fakeResponse.text)
-})
-
-test('getRandomNumberFact error', async () => {
-  getRandomNumberFactService.mockReturnValueOnce(new Error('ups'))
-
-  const result = await getRandomNumberFact()
-  expect(getRandomNumberFactService).toHaveBeenCalledTimes(1)
-  expect(result).toBeInstanceOf(Error)
-})
+test.todo('catchs an error on get random number fact')
